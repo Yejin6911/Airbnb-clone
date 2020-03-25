45625f960a1c61d20da9bd3cc6d1e85e1a41faaa
yejin <cdjin6911@gmail.com>
2020-03-26 00:13:00 +0900

    #6 Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,7 +13,67 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
-    pass
+    fieldsets = (
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "baths")}
+        ),
+        (
+            "Basic Info",
+            {"fields": ("name", "description","country","address","price")}
+        ),
+        (
+            "Times",
+            {"fields": ("check_in","check_out","instant_book")}
+        ),
+        (
+            "More About the Space",
+            {
+                "classes":("collapse",),
+                "fields":("amenities","facilities","house_rules")
+            }
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)}
+        ),
+    )
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+   # ordering = ('name','price','bedrooms')
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city","^host__username",) #foreignkey로 host의 username 접금할 때 다음과 같이 사용
+
+    filter_horizontal = ("amenities","facilities","house_rules",)
+
+    def count_amenities(self, obj):
+        return len(obj.amenities.all())
+
+    count_amenities.short_description = "amenities"
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
