ef1beeab293a12e08e3fdeb43709e798e64e79f4
yejin <cdjin6911@gmail.com>
2020-04-03 00:01:30 +0900

    #8 More Admins!

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -12,11 +13,18 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.rooms.count()
 
 
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
+
+    inlines = (PhotoInline,)
+
     fieldsets = (
         (
             "Spaces",
@@ -56,6 +64,7 @@ class RoomAdmin(admin.ModelAdmin):
         "instant_book",
         "count_amenities",
         "count_photos",
+        "total_rating",
     )
 
    # ordering = ('name','price','bedrooms')
@@ -70,6 +79,8 @@ class RoomAdmin(admin.ModelAdmin):
         "city",
         "country",
     )
+
+    raw_id_fields = ("host",)
 
     search_fields = ("=city","^host__username",) #foreignkey로 host의 username 접금할 때 다음과 같이 사용
 
@@ -86,4 +97,10 @@ class RoomAdmin(admin.ModelAdmin):
 class PhotoAdmin(admin.ModelAdmin):
 
     """ Photo Admin Definition """
-    pass
+
+    list_display = ("__str__","get_thumbnail")
+
+    def get_thumbnail(self,obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
