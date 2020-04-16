8242d257bb5378e8f720a264f79d99fd617683d5
yejin <cdjin6911@gmail.com>
2020-04-16 23:43:16 +0900

    #13.2 Select Choices

--- rooms/views.py
+++ rooms/views.py
@@ -23,8 +23,23 @@ class RoomDetail(DetailView):
     model = models.Room
     pk_url_kwarg = 'potato'
 
+
 def search(request):
-    city=request.GET.get("city", "Anywhere")
-    city=str.capitalize(city)
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
     room_types = models.RoomType.objects.all()
-    return render(request, "rooms/search.html", {"city": city, "countries": countries, "room_types":room_types})
+
+    form= {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+    }
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+
+    }
+    return render(request, "rooms/search.html", {**form, **choices})
