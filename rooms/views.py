1d94994ff0dbd44aa0f258fae9e79e14485f5761
yejin <cdjin6911@gmail.com>
2020-04-18 00:40:27 +0900

    #13.10 Finishing Up!

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,7 @@
 from django.views import View
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
-from django_countries import countries
+from django.core.paginator import Paginator
 from . import models, forms
 
 
@@ -81,9 +81,16 @@ class SearchView(View):
                 for facility in facilities:
                     filter_args["facilities"] = facility
 
-                rooms = models.Room.objects.filter(**filter_args)
-        else:
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
 
+                paginator = Paginator(qs, 10, orphans=5)
+
+                page = request.GET.get("page",1)
+
+                rooms = paginator.get_page(page)
+                return render(request, "rooms/search.html", {"form": form, "rooms":rooms})
+
+        else:
             form = forms.SearchForm()
 
-        return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
+        return render(request, "rooms/search.html", {"form": form})
