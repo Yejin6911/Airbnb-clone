e0267d7816453fddc0cd9e7c18c4063ed6963f80
yejin <cdjin6911@gmail.com>
2020-04-26 02:56:52 +0900

    #14.5 Always an Easier Way

--- users/views.py
+++ users/views.py
@@ -1,17 +1,26 @@
-from django.urls import reverse
 from django.views import View
-from django.shortcuts import render, redirect
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {'email': "test@gmail.com"}
 
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "itn@las.com"})
-        return render(request, "users/login.html", {"form": form})
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
-    def post(self, request):
+    """def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data.get("email")
@@ -20,7 +29,7 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
                 redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+        return render(request, "users/login.html", {"form": form})"""
 
 def log_out(request):
     logout(request)
