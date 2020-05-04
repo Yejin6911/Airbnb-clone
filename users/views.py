51e8389ff212ffeae9b76f5d1bb997dfc02468be
yejin <cdjin6911@gmail.com>
2020-05-04 19:31:39 +0900

    #15.0 Signup Form

--- users/views.py
+++ users/views.py
@@ -20,17 +20,18 @@ class LoginView(FormView):
             login(self.request, user)
         return super().form_valid(form)
 
-    """def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})"""
 
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        'first_name': "Nicos",
+        'last_name': "Serr",
+        'email': "itn@las.com",
+    }
