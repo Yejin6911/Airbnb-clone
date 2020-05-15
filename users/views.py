abfe14fc62afea2d876d03e13eb8af4179a3f547
yejin <cdjin6911@gmail.com>
2020-05-16 02:36:18 +0900

    #17.3 Recap OMG

--- users/views.py
+++ users/views.py
@@ -67,46 +67,44 @@ def github_login(request):
     redirect_uri = "http://localhost:8000/users/login/github/callback"
     return redirect(f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user")
 
+class GithubException(Exception):
+    pass
+
 def github_callback(request):
-    client_id = os.environ.get("GH_ID")
-    client_secret = os.environ.get("GH_SECRET")
-    code = request.GET.get("code", None)
-    if code is not None:
-        result = requests.post(
-            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
-            headers={"Accept":"application/json"},
-        )
-        result_json= result.json()
-        error = result_json.get("error", None)
-        if error is not None:
-            return redirect(reverse("users:login"))
-        else: #이미 해당 소셜로그인으로 로그인가능할 경우
-            access_token = result_json.get("access_token")
-            profile_request = requests.get(
-                "https://api.github.com/user",
-                headers={
-                    "Authorization": f"token {access_token}",
-                    "Accept":"application/json",
-                },
+    try:
+        client_id = os.environ.get("GH_ID")
+        client_secret = os.environ.get("GH_SECRET")
+        code = request.GET.get("code", None)
+        if code is not None:
+            result = requests.post(
+                f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+                headers={"Accept":"application/json"},
             )
-            #로그인으로 들어오는 유저 정보 체크
-            profile_json=profile_request.json()
-            username = profile_json.get('login', None)
-            if username is not None:
-                name = profile_json.get('name')
-                email = profile_json.get('email')
-                bio = profile_json.get('bio')
-                #이미 있는 사용자인지 체크
-                user = models.User.objects.get(email=email)
-                if user is not None:
-                    return redirect(reverse("users:login"))
-                else:
-                    user = models.User.objects.create(
-                        username=email, first_name = name, bio=bio, email=email
-                    )
-                    login(request, user)
-                    return redirect(reverse("core:home"))
-            else:
+            result_json= result.json()
+            error = result_json.get("error", None)
+            if error is not None:
                 return redirect(reverse("users:login"))
-    else:
-        return redirect(reverse("core:home"))
+            else: #이미 해당 소셜로그인으로 로그인가능할 경우
+                access_token = result_json.get("access_token")
+                profile_request = requests.get(
+                    "https://api.github.com/user",
+                    headers={
+                        "Authorization": f"token {access_token}",
+                        "Accept":"application/json",
+                    },
+                )
+                #로그인으로 들어오는 유저 정보 체크
+                profile_json=profile_request.json()
+                username = profile_json.get('login', None)
+                if username is not None:
+                    name = profile_json.get('name')
+                    email = profile_json.get('email')
+                    bio = profile_json.get('bio')
+                    #이미 있는 사용자인지 체크
+                    user = models.User.objects.get(email=email)
+                else:
+                    raise GithubException()
+        else:
+            raise GithubException()
+    except GithubException:
+        return redirect(reverse("user:login"))
