5caba8cad6ca34facac459c124b95c481ae3c52f
yejin <cdjin6911@gmail.com>
2020-05-19 02:11:30 +0900

    #20.0 Sizes in Tailwind-#20.4 Extending Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        //class이름:css로 보내는 이름
+        "25vh" : "25vh",
+        "50vh" : "50vh",
+        "75vh" : "75vh"
+      },
+      borderRadius:{
+        xl: "1.5rem"
+      }
+    },
   },
   variants: {},
   plugins: [],
-}
+};
