1833eb1628d2323ed2de7263d62c3cfcfc867850
yejin <cdjin6911@gmail.com>
2020-07-01 23:57:58 +0900

    #24.10 Model Managers

--- reservations/models.py
+++ reservations/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
+from . import managers
 
 
 class BookedDay(core_models.TimeStampedModel):
@@ -35,6 +36,7 @@ class Reservation(core_models.TimeStampedModel):
     check_out = models.DateField()
     guest = models.ForeignKey("users.User", related_name = "reservations", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", related_name = "reservations", on_delete=models.CASCADE)
+    objects = managers.CustomReservationManager()#manager만들어 사용시 필요
 
     def __str__(self):
         return f'{self.room} - {self.check_in}'
