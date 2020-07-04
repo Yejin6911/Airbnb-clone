59a1147ad84653c11300a5d75a986a92ee5fd8c0
yejin <cdjin6911@gmail.com>
2020-07-04 11:45:16 +0900

    #25.6 Fav Page

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-
+from django.shortcuts import redirect, reverse
+from django.views.generic import TemplateView
 from rooms import models as room_models
 from . import models
 
-def save_room(request, room_pk):
-     room = room_models.Room.objects.get_or_none(pk=room_pk)
-     if room is not None:
-         #tuple로 반환되는 것을 unpack하기 위해 두 변수로 지정
-         the_list, created = models.List.objects.get_or_create(
-             user=request.user, name="My Favorites Houses"
-         )
-         the_list.rooms.add(room)
-         return(redirect(reverse("rooms:detail", kwargs={"pk":room_pk})))
+def toggle_room(request, room_pk):
+    action = request.GET.get("action", None)
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
+    if room is not None and action is not None:
+        #tuple로 반환되는 것을 unpack하기 위해 두 변수로 지정
+        the_list, created = models.List.objects.get_or_create(
+            user=request.user, name="My Favorites Houses"
+        )
+        if action == 'add':
+            the_list.rooms.add(room)
+        elif action == 'remove':
+            the_list.rooms.remove(room)
+        return(redirect(reverse("rooms:detail", kwargs={"pk":room_pk})))
+
+class SeeFavsVeiw(TemplateView):
+
+    template_name = "lists/list_detail.html"
